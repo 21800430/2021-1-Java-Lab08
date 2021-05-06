2ac552f22023b1e8991d6ce81e196306955c9791
21800430 <21800430@handong.edu>
2021-05-06 11:13:41 +0900

    lab08

---
+++ app/src/main/java/edu/handong/csee/java/Lab08/Util.java
@@ -0,0 +1,10 @@
+package edu.handong.csee.java.Lab08;
+
+/**
+ * class
+ */
+public class Util {
+	static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+}
