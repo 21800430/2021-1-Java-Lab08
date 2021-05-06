eeeb5370f0b7bf9303f30cfe3246800298149721
21800430 <21800430@handong.edu>
2021-05-06 16:47:00 +0900

    undo

---
+++ app/src/main/java/edu/handong/csee/java/Lab08/converters/KMToMConverter.java
@@ -0,0 +1,37 @@
+package edu.handong.csee.java.Lab08.converters;
+
+/**
+ * The class to convert km to m
+ */
+public class KMToMConverter implements Convertible {
+	RealConvert con = new RealConvert();
+    private double km, m;
+    
+    /**
+     * The method to get value
+     * @param fromValue
+     */
+	public void setFromValue(double fromValue) {
+		con.setFromValue(fromValue);
+
+	}
+
+	/**
+     * The method to return the converted value
+     * @return m
+     */
+	public double getConvertedValue() {
+		km = con.getConvertedValue();
+        m = km * 1000;
+        return m;
+	}
+
+	/**
+     * The method is worthless
+     */
+	public void convert() {
+		con.Convert();
+
+	}
+
+}
---
+++ app/src/main/java/edu/handong/csee/java/Lab08/converters/TONToKGConverter.java
@@ -0,0 +1,37 @@
+package edu.handong.csee.java.Lab08.converters;
+
+/**
+ * The class to convert ton to kg
+ */
+public class TONToKGConverter implements Convertible {
+	RealConvert con = new RealConvert();
+    private double ton, kg;
+
+    /**
+     * The method to get value
+     * @param fromValue
+     */
+	public void setFromValue(double fromValue) {
+		con.setFromValue(fromValue);
+
+	}
+
+	/**
+     * The method to return the converted value
+     * @return kg
+     */
+	public double getConvertedValue() {
+		ton = con.getConvertedValue();
+        kg = ton * 1000;
+        return kg;
+	}
+
+	/**
+     * The method is worthless
+     */
+	public void convert() {
+		con.Convert();
+
+	}
+
+}
